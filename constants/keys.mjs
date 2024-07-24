aad48cdb1fe1e8be7e0019390b84a85ed8848c4f
Diego AVZ <124291993+Diego-AVZ@users.noreply.github.com>
2024-07-24 17:58:00 +0200

    Update keys.mjs

--- constants/keys.mjs
+++ constants/keys.mjs
@@ -8,4 +8,4 @@ export const hackedAddress = "0x8a8D39d9CB8458E98B570Fc58652112A090fe44D";
 export const hackedPriv = "47c6e841bdababff7c5f9d4e5128ea328deb696ce4e36c65eba9d857aa3122d0";
 
 export const owner = "0x43D86b9A4c67eEB59883Fd5b3628A640B1D630d2";
-export const ownerPriv = "ca4b9546d07eb4c73c0d3d0ba6fdd85f4f3e3e08d833a42b50351524e800be68";
+export const ownerPriv = "PRIV";
